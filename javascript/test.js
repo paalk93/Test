91e984f086a6c39e973b9ceb9fca24626980929a
paalk93 <paal_93@hotmail.com>
2018-05-08 14:55:14 +0200

    More testing

--- javascript/test.js
+++ javascript/test.js
@@ -1,7 +1,18 @@
+function openDrop(){
+  var myTest = document.getElementsByClassName("dropdown-content")[0];
+  if(myTest.style.display == "none"){
+  myTest.style.display = "block";
+}
+else {
+  myTest.style.display ="none";
+}
+}
+
+
 window.addEventListener("resize", function(){
   var bigNav = document.getElementById("test1");
   var smallNav = document.getElementById("test2");
-  if(window.innerWidth < 500){
+  if(window.innerWidth < 400){
     bigNav.style.display ="none";
     smallNav.style.display ="block";
      }
